be57fe7094aff7186c5945693ed8e2c67bdd4bba
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-12-04 00:16:40 -0500

    Commit

---
+++ appemail/android/app/src/main/kotlin/com/example/appemail/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appemail
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ appemail/lib/model/email.dart
@@ -0,0 +1,19 @@
+
+//  Atributos del objeto.
+class Email {
+  final int id;
+  final String from;
+  final String subject;
+  final DateTime dateTime;
+  final String body;
+  bool read;
+
+  Email({
+    required this.id,
+    required this.from,
+    required this.subject,
+    required this.dateTime,
+    required this.body,
+    this.read = false,
+  });
+}
